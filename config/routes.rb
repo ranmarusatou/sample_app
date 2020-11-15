f9333ec1269f559778a213d5c4ea3f9cbb5d3ab7
ranmarusatou <s.rammaru@gmail.com>
2020-11-16 01:55:06 +0900

    Exec: rails generate controller StaticPages home help

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  # => Static Pages#home
+  get 'static_pages/help'
+  # => StaticPages#help
   root  'application#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
