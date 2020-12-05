8ff2429675d2746f28f58b948a611986c666597a
Ranmaru Sato <ranmarusato@ranmarunoair.lan>
2020-12-05 14:39:12 +0900

    Edit view and condtoller files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+# GET /users/new
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email)
+    end
+end
