aef484a860074b51b0ca28824f43b410ae99f1d8
ranmarusatou <s.rammaru@gmail.com>
2020-12-05 22:00:33 +0900

    Change db to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_11_23_020424) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
